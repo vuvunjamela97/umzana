6e406a8fa04b1e786f4d68e9c28087de116704e9
vuvunjamela97 <vuvunjamela97@gmail.com>
2021-02-03 08:34:04 +0200

    working navbar and routing

--- my-website/src/app/app-routing.module.ts
+++ my-website/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'About',
+    component: AboutComponent
+  },
+  {
+    path: 'Contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
